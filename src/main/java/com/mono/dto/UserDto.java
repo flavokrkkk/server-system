ec2136d429ccb5ff0bc2551d64763361b86c17e9
MonoSudji <misupriority@gmail.com>
2024-09-28 01:58:42 +0600

    Add a Notification About Password and a Reg/Log

---
+++ src/main/java/com/mono/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.mono.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private Long id;
+    private String username;
+    private String email;
+
+    public UserDto(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+}
